3a7204295e05a6f7fa04b72610bb99f2bebbb85a
smthnspcl <pascal@eberlein.io>
2020-03-14 17:33:13 +0100

    update

--- selenium_docktor/__init__.py
+++ selenium_docktor/__init__.py
@@ -6,7 +6,7 @@ def get_ip(driver, ip_site="https://api.ipify.org"):
     return driver.find_element_by_tag_name("body").text
 
 
-def make_selenium_instances(manager):
+def make_selenium_instances(manager, headless=False):
     configs = []
     for info in manager.get_containers():
         config = Configuration(proxy=IProxy(
@@ -14,11 +14,9 @@ def make_selenium_instances(manager):
             manager.get_port(info, "8123/tcp"),
             manager.get_port(info, "8118/tcp"),
             manager.get_port(info, "9050/tcp")
-        ))
+        ), headless=headless)
         configs.append(config)
     drivers = []
     for config in configs:
         drivers.append(WebDriver.build(config, False))
     return drivers
-
-
