5ac7dd83307864db44d630f3b53707d59e574495
smthnspcl <pascal@eberlein.io>
2020-03-29 03:03:39 +0200

    update

--- sdt.py
+++ sdt.py
@@ -1,32 +1,31 @@
-from selenium_docktor import get_ip, make_selenium_instances, WebDriver, Configuration
+from selenium_docktor import get_ip, make_selenium_instances, WebDriver
 from docktor import Manager
 
-from loguru import logger
 from time import sleep
 
 
 def main():
-    clearnet_driver = WebDriver.build(Configuration())
-    logger.info("browser w/o proxy: {0}".format(get_ip(clearnet_driver)))
+    clearnet_driver = WebDriver.get_default()
+    print("browser w/o proxy: {0}".format(get_ip(clearnet_driver)))
 
-    logger.info("initializing docktor manager")
+    print("initializing docktor manager")
     manager = Manager(2)
-    logger.info("running docktor manager")
+    print("running docktor manager")
     manager.start()
-    logger.info("waiting for the containers to start")
+    print("waiting for the containers to start")
     sleep(5)
     manager.wait_until_ready()
 
-    logger.info("tor proxy ips:")
-    for tor_driver in make_selenium_instances(manager):
-        logger.info(get_ip(tor_driver))
+    print("tor proxy ips:")
+    for tor_driver in make_selenium_instances(manager, False):
+        print(get_ip(tor_driver))
 
-    logger.info("changing identities")
+    print("changing identities")
     manager.change_all_identities()
 
-    logger.info("new tor proxy ips:")
+    print("new tor proxy ips:")
     for tor_driver in make_selenium_instances(manager):
-        logger.info(get_ip(tor_driver))
+        print(get_ip(tor_driver))
 
     manager.stop()
     manager.join()
